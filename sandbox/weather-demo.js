e39dfa0bb19774e1ba28fe9ec9dfce30fb9050a2
Weston Clark <clark.westonp@gmail.com>
2017-03-15 09:57:21 -0600

    changed city

--- sandbox/weather-demo.js
+++ sandbox/weather-demo.js
@@ -1,11 +1,11 @@
 $(document).ready(function($) {
     $.ajax({
-        url : "http://api.wunderground.com/api/9f4c1d9fbf3832ad/forecast/geolookup/conditions/q/SC/Greenville.json",
+        url : "http://api.wunderground.com/api/9f4c1d9fbf3832ad/forecast/geolookup/conditions/q/TX/Sanantonio.json",
         dataType : "jsonp",
         success : function(data){
             console.log(data);
             var location = data.location.city;
-            var temp_f = data.location.lon;
+            var temp_f = data.current_observation.temp_f;
 
             var loc = $('#h1');
             var temp = $('#h2');
