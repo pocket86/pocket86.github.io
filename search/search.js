6cb8af4afc37ee66fc4ba2540085d30ba520b871
Weston Clark <clark.westonp@gmail.com>
2017-03-22 09:44:43 -0600

    Got the search results page updated. Now working on pulling the weather page results

---
+++ search/search.js
@@ -0,0 +1,24 @@
+$('#query').keyup(function(){
+    // All code will be inside of this block
+    var value = $('#query').val();
+    var rExp = new RegExp(value, "i");
+
+    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+        console.log(data);
+
+        // Begin building output
+        var output = '<ol>';
+        $.each(data.RESULTS, function(key, val) {
+            if (val.name.search(rExp) != -1) {
+                output += '<li>';
+                output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+                output += '</li>';
+            }
+        }); // end each
+        output += '</ol>';
+        $("#searchResults").html(output); // send results to the page
+    }); // end getJSON
+
+
+
+}); // end keyup
