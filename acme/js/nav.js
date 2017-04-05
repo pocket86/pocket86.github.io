6b0d92a45edfe3b4cbf3c9d71ef479b3c976be3c
Weston Clark <clark.westonp@gmail.com>
2017-04-05 03:03:29 -0600

    Got the JQuery pulling the correct page on link click. Now just need to fill in the content :D

--- acme/js/nav.js
+++ acme/js/nav.js
@@ -1,39 +1,39 @@
-function home(){
-    $("#homePage").show();
-    $("#anvilsPage").hide();
-    $("#explosivesPage").hide();
-    $("#decoysPage").hide();
-    $("#trapsPage").hide();
-}
+$("nav").on("click", "a", function (evt) {
+            evt.preventDefault();
+            var id = evt.target.id;
+    
+            if(id === "Home"){
+                $("#homePage").show();
+                $("#dynamicPages").hide();
+                $("#contentTitle").html("Welcome To Acme!");
+            } else{
+                $("#homePage").hide();
+                loadDynamicPage(id);
+            }
+        });
 
-function anvils(){
-    $("#homePage").hide();
-    $("#anvilsPage").show();
-    $("#explosivesPage").hide();
-    $("#decoysPage").hide();
-    $("#trapsPage").hide();
-}
 
-function explosives(){
-    $("#homePage").hide();
-    $("#anvilsPage").hide();
-    $("#explosivesPage").show();
-    $("#decoysPage").hide();
-    $("#trapsPage").hide();
-}
 
-function decoys(){
-    $("#homePage").hide();
-    $("#anvilsPage").hide();
-    $("#explosivesPage").hide();
-    $("#decoysPage").show();
-    $("#trapsPage").hide();
-}
 
-function traps(){
-    $("#homePage").hide();
-    $("#anvilsPage").hide();
-    $("#explosivesPage").hide();
-    $("#decoysPage").hide();
-    $("#trapsPage").show();
+
+function loadDynamicPage(id){
+    
+    $.getJSON("js/acme.json", function (data) {
+        console.log(data);
+        var page;
+        
+        if(id === "Anvils"){
+            page = data.Anvils;
+        } else if(id === "Explosives"){
+            page = data.Explosives;
+        } else if(id === "Decoys"){
+            page = data.Decoys;
+        } else {
+            page = data.Traps;
+        }
+        
+        //change the page content
+        $("#contentTitle").html(page.name);
+     
+    }); // end getJSON
 }
